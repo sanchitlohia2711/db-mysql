6aee0504979975ca66539ed5a8c6a42c42641d60
sanchitlohia <sanchitlohia27@gmail.com>
2018-12-16 21:44:39 +0700

    changes

--- db/mysql.go
+++ db/mysql.go
@@ -17,8 +17,7 @@ const (
 )
 
 var (
-	engine     = &xorm.Engine{}
-	gormEngine = &gorm.DB{}
+	engine = &xorm.Engine{}
 )
 
 //MySQLParamsStruct : mysql params struct
@@ -31,7 +30,7 @@ type MySQLParamsStruct struct {
 }
 
 //Initialize : intialize db
-func Initialize(p *MySQLParamsStruct) (err error) {
+func Initialize(p *MySQLParamsStruct) (gormDB *gorm.DB, err error) {
 	return InitializeGorm(p)
 }
 
@@ -49,24 +48,18 @@ func InitializeXorm(p *MySQLParamsStruct) (err error) {
 }
 
 //InitializeGorm initilize the gorm
-func InitializeGorm(p *MySQLParamsStruct) (err error) {
+func InitializeGorm(p *MySQLParamsStruct) (gormDB *gorm.DB, err error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		p.Username, p.Password, p.Hostname, p.Schema)
-	gormEngine, err = gorm.Open(MySQLDriverName, dsn)
+	gormDB, err = gorm.Open(MySQLDriverName, dsn)
 	if err != nil {
 		err = fmt.Errorf(ErrFailedToConnectToSQL, err.Error())
 		return
 	}
-	defer gormEngine.Close()
 	return
 }
 
 //NewSQLSession : create new sql session
 func NewSQLSession() *xorm.Session {
 	return engine.NewSession()
-}
-
-//GormEngine return new gorm engine
-func GormEngine() *gorm.DB {
-	return gormEngine
 }
